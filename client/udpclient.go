5b53bdbf60e65ed8499e495768370b4e1b34a7f7
wanghaijun <wanghaijun@ztgame.com>
2016-03-22 01:56:19 +0800

    udp测试

--- client/udpclient.go
+++ client/udpclient.go
@@ -8,6 +8,11 @@ import (
 	"time"
 )
 
+var (
+	recvHeadCh = make(chan *UdpHeader, 1024)
+	HEADLEN    = 4
+)
+
 type UdpHeader struct {
 	datasize  byte
 	bitmask   byte
@@ -29,42 +34,53 @@ func (self *UdpHeader) Serialize() []byte {
 }
 
 func (self *UdpHeader) Unserialize(b []byte, all int) int {
-	buf := bytes.NewBuffer(b)
-	self.datasize, _ = buf.ReadByte()
-	self.bitmask, _ = buf.ReadByte()
+	self.datasize = b[0]
+	self.bitmask = b[1]
 	if all < int(self.datasize)+6 {
 		fmt.Println("Unserialize: ", all, int(self.datasize))
 		return 0
 	}
-	seq := make([]byte, 2)
-	ack := make([]byte, 2)
-	buf.Read(seq)
-	buf.Read(ack)
-	self.seq = uint16(int(seq[0])<<6 + int(seq[1]))
-	self.ack = uint16(int(ack[0])<<6 + int(ack[1]))
+	seq := b[2:4]
+	ack := b[4:6]
+	self.seq = uint16((int(seq[1]) << 8) + int(seq[0]))
+	self.ack = uint16((int(ack[1]) << 8) + int(ack[0]))
 	if self.datasize > 0 {
-		var err error
-		self.data, err = buf.ReadBytes(self.datasize)
-		if err != nil {
-			fmt.Println(err, self.datasize, buf.Len())
-		}
+		self.data = b[6 : 6+self.datasize]
 	}
-	fmt.Println(string(seq), string(ack), self.seq, self.ack, string(self.data))
+	fmt.Println(self.seq, self.ack, string(self.data))
 	return 6 + int(self.datasize)
 }
 
 type UdpData struct {
 	curseq uint16
 	lastok uint16
+	maxok  uint16
 	curack uint16
 	header [65536]*UdpHeader
-	wait   *bytes.Buffer
 }
 
-func (self *UdpData) SendData(b []byte) bool {
+type Server struct {
+	conn     *net.UDPConn
+	addr     *net.UDPAddr
+	recvData *UdpData
+	sendData *UdpData
+	wait     *bytes.Buffer
+}
+
+func (self *Server) CheckSendWaitData() bool {
+	if self.wait == nil {
+		return true
+	}
+	wait := self.wait
+	self.wait = nil
+	return self.SendData(wait.Bytes())
+	return true
+}
+
+func (self *Server) SendData(b []byte) bool {
 	bsize := len(b)
 	for cur := 0; cur < bsize; {
-		if self.curseq+1 == self.lastok {
+		if self.sendData.curseq+1 == self.sendData.lastok {
 			if self.wait == nil {
 				self.wait = bytes.NewBuffer(nil)
 			}
@@ -72,8 +88,8 @@ func (self *UdpData) SendData(b []byte) bool {
 			return false
 		}
 		head := &UdpHeader{}
-		head.seq = self.curseq
-		head.timestamp = int64(time.Now().UnixNano() / time.Millisecond.Nanoseconds())
+		head.seq = self.sendData.curseq
+		head.timestamp = int64(time.Now().UnixNano() / int64(time.Millisecond))
 		if bsize >= cur+256 {
 			head.data = b[cur : cur+256]
 			cur += 256
@@ -81,19 +97,106 @@ func (self *UdpData) SendData(b []byte) bool {
 			head.data = b[cur:bsize]
 			cur = bsize
 		}
-		self.header[self.curseq] = head
-		self.curseq++
-		if self.curseq == 65535 {
-			self.curseq = 0
+		self.conn.Write(head.Serialize())
+		self.sendData.curseq++
+		if self.sendData.curseq == 65535 {
+			self.sendData.curseq = 0
 		}
 	}
 	return true
 }
 
-type UdpClient struct {
-	conn     *net.UDPConn
-	recvData *UdpData
-	sendData *UdpData
+func (self *Server) Loop() {
+	timersend := time.NewTicker(time.Millisecond)
+	for {
+		select {
+		case head := <-recvHeadCh:
+			if head.datasize == 0 {
+				ismax := false
+				if head.seq > self.sendData.maxok {
+					ismax = true
+					self.sendData.maxok = head.seq
+				}
+				if head.bitmask&1 == 1 {
+					if self.sendData.lastok < head.seq {
+						for i := self.sendData.lastok + 1; i <= head.seq; i++ {
+							self.sendData.header[i] = nil
+						}
+					}
+				} else {
+					self.sendData.header[head.seq] = nil
+				}
+				if self.sendData.lastok < self.sendData.maxok {
+					//这里尽量保证丢包后不要被多次重发,但是还是很难避免
+					if ismax {
+						for i := self.sendData.lastok + 1; i <= self.sendData.maxok; i++ {
+							if self.sendData.header[i] != nil {
+								//发现有更新的包已经确认,所有老包直接重发
+								//self.sendData.header[i] = int64(time.Now().UnixNano() / time.Millisecond.Nanoseconds())
+								self.conn.Write(self.sendData.header[i].Serialize())
+								fmt.Println("丢包重发")
+							}
+						}
+					}
+					for i := self.sendData.lastok + 1; i <= self.sendData.maxok; i++ {
+						if self.sendData.header[i] != nil {
+							break
+						}
+						self.sendData.lastok = i
+					}
+				}
+			}
+			if head.seq >= self.recvData.lastok && self.recvData.header[head.seq] == nil {
+				self.recvData.header[head.seq] = head
+				if head.seq > self.recvData.maxok {
+					self.recvData.maxok = head.seq
+				}
+				for i := self.recvData.lastok; i <= self.recvData.maxok; i++ {
+					if self.recvData.header[i] == nil {
+						break
+					}
+					self.recvData.lastok = i
+				}
+			} else {
+				//收到过期数据,说明对方没有收到确认包,发一个
+				head.datasize = 0
+				self.conn.Write(head.Serialize())
+				fmt.Println("收到过期数据包", head.seq, self.recvData.lastok)
+			}
+		case <-timersend.C:
+			if self.recvData.curack < self.recvData.lastok {
+				fmt.Println("wwww", self.recvData.curack, self.recvData.lastok, self.recvData.maxok)
+				head := &UdpHeader{}
+				head.seq = self.recvData.lastok
+				head.bitmask |= 1
+				self.conn.Write(head.Serialize())
+				self.recvData.curack = self.recvData.lastok
+				self.recvData.header[self.recvData.curack].seq = 0
+			}
+			for i := self.recvData.curack; i <= self.recvData.maxok; i++ {
+				if self.recvData.header[i] != nil && self.recvData.header[i].seq != 0 {
+					head := &UdpHeader{}
+					head.seq = self.recvData.header[i].seq
+					self.conn.Write(head.Serialize())
+					self.recvData.header[i].seq = 0
+				}
+			}
+			if self.sendData.lastok < self.sendData.maxok {
+				for i := self.sendData.lastok + 1; i <= self.sendData.maxok; i++ {
+					if self.sendData.header[i] != nil && int64(time.Now().UnixNano()/int64(time.Millisecond)) > self.sendData.header[i].timestamp+2000 {
+						//发现有更新的包已经确认,所有老包直接重发
+						self.sendData.header[i].timestamp = int64(time.Now().UnixNano() / int64(time.Millisecond))
+						self.conn.Write(self.sendData.header[i].Serialize())
+						fmt.Println("超时重发")
+					}
+				}
+			}
+			if self.sendData.curseq < 10 {
+				self.SendData([]byte("wanghaijun"))
+			}
+			self.CheckSendWaitData()
+		}
+	}
 }
 
 func main() {
@@ -102,27 +205,53 @@ func main() {
 		fmt.Println("net.ResolveUDPAddr err:", err)
 		return
 	}
-	c := &UdpClient{
+	s := &Server{
 		recvData: &UdpData{},
 		sendData: &UdpData{},
 	}
-	c.conn, err = net.DialUDP("udp", nil, addr)
+	s.conn, err = net.DialUDP("udp", nil, addr)
 	if err != nil {
 		fmt.Println("net.DialUDP err:", err)
 		return
 	}
-	for i := 0; i < 1000; i++ {
-		c.sendData.SendData([]byte("wanghaijun"))
-		for j := c.sendData.lastok; j <= c.sendData.curseq; j++ {
-			head := c.sendData.header[j]
-			if head != nil {
-				fmt.Println("sendData: ", i, head.seq, c.sendData.curseq, len(head.Serialize()))
-				n, err := c.conn.Write(head.Serialize())
-				if err != nil {
-					fmt.Println("ERROR: ", err, n)
-					return
+	s.addr = addr
+	go s.Loop()
+	/*
+		for i := 0; i < 1000; i++ {
+			c.sendData.SendData([]byte("wanghaijun"))
+			for j := c.sendData.lastok; j <= c.sendData.curseq; j++ {
+				head := c.sendData.header[j]
+				if head != nil {
+					fmt.Println("sendData: ", i, head.seq, c.sendData.curseq, len(head.Serialize()))
+					n, err := c.conn.Write(head.Serialize())
+					if err != nil {
+						fmt.Println("ERROR: ", err, n)
+						return
+					}
 				}
 			}
+		}
+		// */
+	b := make([]byte, 1024)
+	left := 0
+	for {
+		n, addr, err := s.conn.ReadFromUDP(b[left:])
+		if err != nil {
+			fmt.Println("ERROR: ", err, n, addr)
+			return
+		}
+		all := n + left
+		for all >= 6 {
+			head := &UdpHeader{}
+			offset := head.Unserialize(b, all)
+			if offset > 0 {
+				//fmt.Println("ReadFromUDP: ", left, n, all, int(head.datasize))
+				copy(b, b[offset:all])
+				all -= offset
+			} else {
+				break
+			}
+			recvHeadCh <- head
 		}
 	}
 }
--- server/udpserver.go
+++ server/udpserver.go
@@ -9,8 +9,7 @@ import (
 )
 
 var (
-	recvHeadCh chan UdpHeader
-	sendDataCh chan []byte
+	recvHeadCh = make(chan *UdpHeader, 1024)
 	HEADLEN    = 4
 )
 
@@ -55,24 +54,26 @@ func (self *UdpHeader) Unserialize(b []byte, all int) int {
 type UdpData struct {
 	curseq uint16
 	lastok uint16
+	maxok  uint16
 	curack uint16
 	header [65536]*UdpHeader
-	wait   *bytes.Buffer
 }
 
 type Server struct {
 	conn     *net.UDPConn
+	addr     *net.UDPAddr
 	recvData *UdpData
 	sendData *UdpData
+	wait     *bytes.Buffer
 }
 
 func (self *Server) CheckSendWaitData() bool {
-	if self.sendData.wait == nil {
+	if self.wait == nil {
 		return true
 	}
-	wait := self.sendData.wait
-	self.sendData.wait = nil
-	return self.sendData.SendData(wait.Bytes())
+	wait := self.wait
+	self.wait = nil
+	return self.SendData(wait.Bytes())
 	return true
 }
 
@@ -80,15 +81,15 @@ func (self *Server) SendData(b []byte) bool {
 	bsize := len(b)
 	for cur := 0; cur < bsize; {
 		if self.sendData.curseq+1 == self.sendData.lastok {
-			if self.sendData.wait == nil {
-				self.sendData.wait = bytes.NewBuffer(nil)
+			if self.wait == nil {
+				self.wait = bytes.NewBuffer(nil)
 			}
-			self.sendData.wait.Write(b[cur:])
+			self.wait.Write(b[cur:])
 			return false
 		}
 		head := &UdpHeader{}
 		head.seq = self.sendData.curseq
-		head.timestamp = int64(time.Now().UnixNano() / time.Millisecond.Nanoseconds())
+		head.timestamp = int64(time.Now().UnixNano() / int64(time.Millisecond))
 		if bsize >= cur+256 {
 			head.data = b[cur : cur+256]
 			cur += 256
@@ -96,7 +97,7 @@ func (self *Server) SendData(b []byte) bool {
 			head.data = b[cur:bsize]
 			cur = bsize
 		}
-		self.conn.Write(head.Serialize())
+		self.conn.WriteToUDP(head.Serialize(), self.addr)
 		self.sendData.curseq++
 		if self.sendData.curseq == 65535 {
 			self.sendData.curseq = 0
@@ -106,31 +107,90 @@ func (self *Server) SendData(b []byte) bool {
 }
 
 func (self *Server) Loop() {
-	timersend := time.NewTimer(Millisecond)
+	timersend := time.NewTicker(time.Millisecond)
 	for {
 		select {
 		case head := <-recvHeadCh:
-			if head.seq > self.recvData.lastok {
-				self.recvData[head.seq] = head
-				for i := self.recvData.lastok; i <= head.seq; i++ {
-					if self.recvData.header[i] != nil {
-						self.recvData.lastok++
+			if head.datasize == 0 {
+				ismax := false
+				if head.seq > self.sendData.maxok {
+					ismax = true
+					self.sendData.maxok = head.seq
+				}
+				if head.bitmask&1 == 1 {
+					if self.sendData.lastok < head.seq {
+						for i := self.sendData.lastok + 1; i <= head.seq; i++ {
+							self.sendData.header[i] = nil
+						}
 					}
+				} else {
+					self.sendData.header[head.seq] = nil
+				}
+				if self.sendData.lastok < self.sendData.maxok {
+					//这里尽量保证丢包后不要被多次重发,但是还是很难避免
+					if ismax {
+						for i := self.sendData.lastok + 1; i <= self.sendData.maxok; i++ {
+							if self.sendData.header[i] != nil {
+								//发现有更新的包已经确认,所有老包直接重发
+								//self.sendData.header[i] = int64(time.Now().UnixNano() / time.Millisecond.Nanoseconds())
+								self.conn.WriteToUDP(self.sendData.header[i].Serialize(), self.addr)
+								fmt.Println("丢包重发")
+							}
+						}
+					}
+					for i := self.sendData.lastok + 1; i <= self.sendData.maxok; i++ {
+						if self.sendData.header[i] != nil {
+							break
+						}
+						self.sendData.lastok = i
+					}
+				}
+			}
+			if head.seq >= self.recvData.lastok && self.recvData.header[head.seq] == nil {
+				self.recvData.header[head.seq] = head
+				if head.seq > self.recvData.maxok {
+					self.recvData.maxok = head.seq
+				}
+				for i := self.recvData.lastok; i <= self.recvData.maxok; i++ {
+					if self.recvData.header[i] == nil {
+						break
+					}
+					self.recvData.lastok = i
 				}
 			} else {
-				fmt.Println("收到过期数据包")
+				//收到过期数据,说明对方没有收到确认包,发一个
+				head.datasize = 0
+				self.conn.WriteToUDP(head.Serialize(), self.addr)
+				fmt.Println("收到过期数据包", head.seq, self.recvData.lastok)
 			}
-			if head.ack != 0 {
-				self.sendData.header[head.ack] = nil
-				for i := self.sendData.lastok; i <= head.ack; i++ {
-					if self.sendData.header[i] == nil {
-						self.sendData.lastok++
+		case <-timersend.C:
+			if self.recvData.curack < self.recvData.lastok {
+				fmt.Println("wwww", self.recvData.curack, self.recvData.lastok, self.recvData.maxok)
+				head := &UdpHeader{}
+				head.seq = self.recvData.lastok
+				head.bitmask |= 1
+				self.conn.WriteToUDP(head.Serialize(), self.addr)
+				self.recvData.curack = self.recvData.lastok
+				self.recvData.header[self.recvData.curack].seq = 0
+			}
+			for i := self.recvData.curack; i <= self.recvData.maxok; i++ {
+				if self.recvData.header[i] != nil && self.recvData.header[i].seq != 0 {
+					head := &UdpHeader{}
+					head.seq = self.recvData.header[i].seq
+					self.conn.WriteToUDP(head.Serialize(), self.addr)
+					self.recvData.header[i].seq = 0
+				}
+			}
+			if self.sendData.lastok < self.sendData.maxok {
+				for i := self.sendData.lastok + 1; i <= self.sendData.maxok; i++ {
+					if self.sendData.header[i] != nil && int64(time.Now().UnixNano()/int64(time.Millisecond)) > self.sendData.header[i].timestamp+2000 {
+						//发现有更新的包已经确认,所有老包直接重发
+						self.sendData.header[i].timestamp = int64(time.Now().UnixNano() / int64(time.Millisecond))
+						self.conn.WriteToUDP(self.sendData.header[i].Serialize(), self.addr)
+						fmt.Println("超时重发")
 					}
 				}
 			}
-		case data := <-sendDataCh:
-			self.sendData.wait.Write(data)
-		case <-timersend.C:
 			self.CheckSendWaitData()
 		}
 	}
@@ -147,6 +207,7 @@ func main() {
 		fmt.Println("net.ListenUDP err:", err)
 		return
 	}
+	go s.Loop()
 	b := make([]byte, 1024)
 	left := 0
 	for {
@@ -155,6 +216,7 @@ func main() {
 			fmt.Println("ERROR: ", err, n, addr)
 			return
 		}
+		s.addr = addr
 		all := n + left
 		for all >= 6 {
 			head := &UdpHeader{}
