85fb04bf844065c5f847f182cc044721f01615d2
wanghaijun <wanghaijun@ztgame.com>
2016-03-25 00:14:38 +0800

    udp消息

--- udp/udp.go
+++ udp/udp.go
@@ -236,7 +236,7 @@ func (self *UdpTask) Loop() {
 					//fmt.Println("确认包完成", head.seq)
 					if self.sendData.header[head.seq] != nil {
 						self.sendData.header[head.seq].time_ack = now
-						self.sendData.header[head.seq] = nil
+						//self.sendData.header[head.seq] = nil
 					}
 				}
 				//这里尽量保证丢包后不要被多次重发,但是还是很难避免
